24d1b1d984c11f989bb0e7f63f1d44e18b787da1
git-aniket <mazumder_aniket@hotmail.com>
2022-03-23 13:50:43 +0100

    implemented a repeating timer call at 100Hz for the IMU readouts.

--- i2c_mpu9250/main.cpp
+++ i2c_mpu9250/main.cpp
@@ -9,27 +9,48 @@
 #define I2C_SDA 8
 #define I2C_SCL 9
 
+volatile bool timer_fired = false;
+
+bool repeating_timer_callback(struct repeating_timer *t)
+{
+    timer_fired = true;
+    return true;
+}
+
 /**
- * @brief 
- * @return int 
+ * @brief
+ * @return int
  */
 int main()
 {
+    // timer setup
+    struct repeating_timer timer;
+    add_repeating_timer_ms(10, repeating_timer_callback, NULL, &timer);
+
     sleep_ms(100);
     stdio_init_all();
     sleep_ms(100);
 
-    //initialize sensor
-    MPU9250 MPU(i2c0,I2C_SDA, I2C_SCL,ACC_FS_2G,GYRO_FS_1000DPS);
-    printf("Legend\n");
+    // initialize sensor
+    MPU9250 MPU(i2c0, I2C_SDA, I2C_SCL, ACC_FS_4G, GYRO_FS_1000DPS);
     printf("AccelX AccelY AccelZ GyroX GyroY GyroZ\n");
 
     while (1)
     {
-	//printf("%f,%f,%f,%f,%f,%f\n",MPU.get_accel(ACCEL_X),MPU.get_accel(ACCEL_Y),MPU.get_accel(ACCEL_Z), MPU.get_gyro(GYRO_X),MPU.get_gyro(GYRO_Y),MPU.get_gyro(GYRO_Z));
-	//printf("%f,%f,%f,%f,%f,%f\n",MPU.get_accel_g(ACCEL_X),MPU.get_accel_g(ACCEL_Y),MPU.get_accel_g(ACCEL_Z),MPU.get_gyro_dps(GYRO_X),MPU.get_gyro_dps(GYRO_Y),MPU.get_gyro_dps(GYRO_Z));
-	printf("%f\n",MPU.get_gyro_dps(GYRO_Z));
-	sleep_ms(10);
+        tight_loop_contents();
+        if (timer_fired == true)
+        {
+            timer_fired = false;
+            printf("%f,%f,%f,%f,%f,%f\n",
+                   MPU.get_accel_g(ACCEL_X),
+                   MPU.get_accel_g(ACCEL_Y),
+                   MPU.get_accel_g(ACCEL_Z),
+                   MPU.get_gyro_dps(GYRO_X),
+                   MPU.get_gyro_dps(GYRO_Y),
+                   MPU.get_gyro_dps(GYRO_Z));
+        //printf("%f,%f,%f\n", MPU.get_accel_g(ACCEL_X), MPU.get_accel_g(ACCEL_Y), MPU.get_accel_g(ACCEL_Z));
+        //printf("%f,%f,%f\n", MPU.get_gyro_dps(GYRO_X), MPU.get_gyro_dps(GYRO_Y), MPU.get_gyro_dps(GYRO_Z));
+        }
     }
 
     /*Should not come to this point during normal operation*/
